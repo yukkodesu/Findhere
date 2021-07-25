d612df46d4743eb5df65c10ce6c6d451ae8373c2
wisper12933 <2738280858@qq.com>
2021-07-25 12:43:50 +0800

    Add profile activity

--- app/src/main/java/com/project/findhere/MainActivity.kt
+++ app/src/main/java/com/project/findhere/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.project.findhere
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import com.project.findhere.R.id.mainSpace as mainSpace1
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +23,14 @@ class MainActivity : AppCompatActivity() {
             it.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
         }
         val navView : NavigationView = findViewById(R.id.main_navView)
-        navView.setCheckedItem(R.id.mainSpace)
+        navView.setCheckedItem(mainSpace1)
         navView.setNavigationItemSelectedListener {
+
+            if (it.itemId == R.id.mainSpace){
+                val intent = Intent(this,ProfileActivity::class.java)
+                startActivity(intent)
+            }
+
             val drawerLayout : DrawerLayout = findViewById(R.id.drawerlayout)
             drawerLayout.closeDrawers()
             true
@@ -45,4 +54,5 @@ class MainActivity : AppCompatActivity() {
         }
         return true
     }
+
 }
